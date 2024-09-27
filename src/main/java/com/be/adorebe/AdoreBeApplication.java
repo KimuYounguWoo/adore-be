90ebf70b8bcc70bb43d4fecde6365ddabbfbf663
Kimuyoungwoo <dkp0803@gmail.com>
2024-09-27 22:12:26 +0900

    Docker Build

--- src/main/java/com/be/adorebe/AdoreBeApplication.java
+++ src/main/java/com/be/adorebe/AdoreBeApplication.java
@@ -8,6 +8,7 @@ public class AdoreBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdoreBeApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
